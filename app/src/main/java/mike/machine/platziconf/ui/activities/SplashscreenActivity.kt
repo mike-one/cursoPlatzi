d643a8ad09aa4a076aeb59cc3e051bdc4b1fd457
Miguel Acosta <migue.un.av@gmail.com>
2020-12-01 18:40:46 -0600

    Subiendo el proyecto del curso de Kotlin para Android

---
+++ app/src/main/java/mike/machine/platziconf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package mike.machine.platziconf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import mike.machine.platziconf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
